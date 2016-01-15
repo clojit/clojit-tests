dd2f29228fb60cf959409b9bce4bae6dd47bc499
Raphael Theiler <rtheiler@esheep.ch>
2016-01-15 11:48:15 +0100

    aded more debug output

--- src/test/java/ReferenceCompareTest.java
+++ src/test/java/ReferenceCompareTest.java
@@ -41,6 +41,27 @@ public class ReferenceCompareTest {
         Runtime rt = Runtime.getRuntime();
         String[] commands = {"sh","-c","chmod u+x setupsh && ./setup.sh"};
         Process proc = rt.exec(commands);
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(proc.getErrorStream()));
+
+        // read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        StringBuilder sb = new StringBuilder();
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+            sb.append(s);
+        }
+
+        // read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((s = stdError.readLine()) != null) {
+            System.err.println(s);
+            sb.append(s);
+        }
     }
 
     @Test public void anotherSample() throws IOException{
